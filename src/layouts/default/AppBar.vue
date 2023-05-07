34d1e6e829374ff0e0244479da75be0bec3a52cc
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-07 16:27:11 +0200

    add version

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,12 +1,16 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      RELMA 2023 - Group 7 -
+      RELMA 2023 - Group 7 - Version: {{ version }}
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-//import getVersion from 'relma23-team7-version-util-lib-trial';
+import VersionUtil from 'relma23-team7-version-util-lib-trial';
+import {ref} from "vue";
+
+const version = ref('');
+version.value = VersionUtil.getVersion();
 
 </script>
